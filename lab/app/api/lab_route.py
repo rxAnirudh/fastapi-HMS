5891ccad21e606775c9e770bb3cad60c1e452956
rxAnirudh <radixdt.2336@gmail.com>
2022-08-03 15:20:53 +0530

    🚀 Done with crud operation for lab table in hospital management project

---
+++ lab/app/api/lab_route.py
@@ -0,0 +1,37 @@
+"""File for patient route"""
+import sys
+sys.path.append('/Users/anirudh.chawla/python_fast_api_projects/hospital-management-fastapi')
+from fastapi import Depends,  HTTPException, APIRouter
+from sqlalchemy.orm import Session
+from lab.app.models import schemas
+from db import get_db
+
+from api import controller
+
+lab_router = APIRouter()
+
+
+@lab_router.post("/add_lab", response_model=schemas.AddLabResponse)
+def add_bill(lab: schemas.LabBase, database: Session = Depends(get_db)):
+    """Function to return final response while adding new lab details"""
+    return controller.add_new_lab(database,lab)
+
+
+@lab_router.post("/get_lab_details")
+def get_lab_details(labid: schemas.LabId, database: Session = Depends(get_db)):
+    """Function to return lab details
+    (specific and all lab data can be fetched)"""
+    return controller.get_lab_by_id(database, id = labid.id)
+
+
+@lab_router.post("/delete_lab_details")
+def delete_lab_details(labid: schemas.LabId, database: Session = Depends(get_db)):
+    """Function to return lab details
+    (specific and all lab data can be fetched)"""
+    return controller.delete_lab_details(database, id = labid.id)
+
+
+@lab_router.post("/update_lab_details")
+def update_lab_details(lab_details: schemas.AddNewLab, database: Session = Depends(get_db)):
+    """Function to update particular lab details"""
+    return controller.update_lab_details(database, lab = lab_details)
---
+++ lab/app/models/schemas.py
@@ -0,0 +1,52 @@
+"""Schema file for patient table"""
+from asyncio.log import logger
+import re
+from typing import Optional
+from pydantic import BaseModel, validator,EmailStr
+from datetime import datetime
+
+class LabBase(BaseModel):
+    """Base class model for lab"""
+    staff_id: Optional[str] = None
+    patient_id: Optional[str] = None
+    test_type: Optional[str] = None
+    test_code: Optional[str] = None
+    weight: Optional[str] = None
+    height: Optional[str] = None
+    blood_pressure: Optional[str] = None
+    test_result: Optional[str] = None
+    created_at: Optional[str] = str(datetime.utcnow())
+    updated_at: Optional[str] = str(datetime.utcnow())
+    hospital_id: Optional[str] = None
+    
+class AddNewLab(LabBase):
+    """Create class model for lab"""
+    id : int
+    staff_id: Optional[str] = None
+    patient_id: Optional[str] = None
+    test_type: Optional[str] = None
+    test_code: Optional[str] = None
+    weight: Optional[str] = None
+    height: Optional[str] = None
+    blood_pressure: Optional[str] = None
+    test_result: Optional[str] = None
+    created_at: Optional[str] = str(datetime.utcnow())
+    updated_at: Optional[str] = str(datetime.utcnow())
+    hospital_id: Optional[str] = None
+
+class AddLabResponse(BaseModel):
+    """Create class model for response of new lab to be added"""
+    data : AddNewLab
+    success : bool
+    message : str
+
+class GetLabDetailsResponse(BaseModel):
+    """Create class model for response of specific lab details"""
+    data : AddNewLab
+    success : bool
+    message : str
+
+
+class LabId(BaseModel):
+    """Create class model for requesting id param in get lab api"""
+    id : Optional[int] = None
